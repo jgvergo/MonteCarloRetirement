e092b24065c0efdd77a6dcc689be912c1e872958
John Vergo <38325982+jgvergo@users.noreply.github.com>
2020-11-30 10:21:27 -0500

    Clean up comments

--- Simulation/main/routes.py
+++ Simulation/main/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from Simulation.models import Post, Scenario
+from Simulation.models import Scenario
 
 
 main = Blueprint('main', __name__)
@@ -9,8 +9,7 @@ main = Blueprint('main', __name__)
 @main.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-#    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    scenarios = Scenario.query.order_by(Scenario.date_posted.desc()).paginate(page=page, per_page=5)
+    scenarios = Scenario.query.order_by(Scenario.date_posted.desc()).paginate(page=page, per_page=3)
     return render_template('home.html', scenarios=scenarios)
 
 
