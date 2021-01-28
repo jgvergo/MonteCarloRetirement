f8c943ff4e326b7fedd68301b291e894805edc31
John Vergo <38325982+jgvergo@users.noreply.github.com>
2021-01-28 14:45:04 -0500

    Prevented double loading of the app in debug mode

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from Simulation import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # use_reloader=False prevents create_app from being called twice
+    app.run(debug=True, use_reloader=False)
