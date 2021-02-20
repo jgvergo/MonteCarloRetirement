bacc77e33251c13c81efdcea9cd36404b5e3b34b
John Vergo <38325982+jgvergo@users.noreply.github.com>
2021-02-20 16:43:33 -0500

    Turn off SQLAlchemy tracking of modifications

--- Simulation/config.py
+++ Simulation/config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
