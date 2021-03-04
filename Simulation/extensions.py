8459dfb33265f6385646a9b5531386cae0ee0bda
John Vergo <38325982+jgvergo@users.noreply.github.com>
2021-03-04 13:44:11 -0500

    Added rq background processing

--- Simulation/extensions.py
+++ Simulation/extensions.py
@@ -2,6 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bcrypt import Bcrypt
+from rq import Queue
+from redis import Redis
 
 
 bcrypt = Bcrypt()
@@ -10,3 +12,6 @@ login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
 db = SQLAlchemy()
+# Tell RQ what Redis connection to use
+redis_conn = Redis()
+q = Queue(connection=redis_conn)
