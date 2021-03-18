5ced38372c00e73ca60b1362fa37ff8858116683
John Vergo <38325982+jgvergo@users.noreply.github.com>
2021-03-18 12:32:01 -0400

    Restructured the "Run All" logic so that the background process does not access the database and the only data passed back from the background process (via Redis) is the scenario and a dataframe with the summary Run All statistics (minimizing the sire of the Redis store required for the feature)

--- Simulation/MCSim.py
+++ Simulation/MCSim.py
@@ -17,7 +17,13 @@ class RunSimParams():
         self.sd = None
         self.num_sims = None
         self.sim_num = None
-        self.investments = None
+
+        # Title and type used for "Run All"
+        self.title = None
+        self.type = None
+
+    def __str__(self):
+        return " % s,  % s" % (self.title, self.type)
 
 
 def run_sim_background(scenario, assetmix):
@@ -29,8 +35,9 @@ def run_sim_background(scenario, assetmix):
     rsp.num_sims = 1
     rsp.sim_num = 0
     rsp.investments = get_invest_data(scenario.asset_mix_id, assetmix)
-
-    job = q.enqueue(_run_sim_background, rsp, job_timeout=6000)
+    rsp_list = []
+    rsp_list.append(rsp)
+    job = q.enqueue(_run_sim_background, rsp_list, job_timeout=6000)
 
     if sd.debug:
         registry = FailedJobRegistry(queue=q)
@@ -44,14 +51,14 @@ def run_sim_background(scenario, assetmix):
 
 
 # This routine (and any called routine from the routine) run as part of the Worker.py process
-def _run_sim_background(rsp):
+def _run_sim_background(rsp_list):
     from rq import get_current_job
-
+    rsp = rsp_list[0]
     job = get_current_job()
     rsp.job = job
     if rsp.sd.debug:
         p0_output, fd_output, rs_output, ss_output, sss_output, inv_output, inf_output, sd_output, cola_output = \
-        _run_simulation(rsp)
+            _run_simulation(rsp)
 
         return rsp.scenario, p0_output, fd_output, rs_output, ss_output, sss_output, inv_output, inf_output, sd_output, cola_output
 
@@ -61,12 +68,48 @@ def _run_sim_background(rsp):
         return rsp.scenario, p0_output, fd_output, rs_output, ss_output, sss_output
 
 
-
 def run_all_sim_background(scenario, assetmix):
-    if scenario.author != current_user:
-        abort(403)
-    job = q.enqueue(_run_all_sim_background, scenario, assetmix, job_timeout=6000)
     sd = SimData.query.first()
+
+    # Build the list of run_simulation parameters
+    num_sims = len(AssetMix.query.all()) + len(AssetClass.query.all())
+    rsp_list = []
+    sim_num = 0
+
+    # First do the AssetMixes
+    asset_mix_list = AssetMix.query.order_by(AssetMix.title.asc()).all()
+    for i, asset_mix in enumerate(asset_mix_list):
+        scenario.asset_mix_id = asset_mix.id
+        rsp = RunSimParams()
+        rsp.sim_num = sim_num
+        rsp.investments = get_invest_data(scenario.asset_mix_id, assetmix)
+        rsp.type = 'Asset Mix'
+        rsp.title = asset_mix.title
+
+        # Only need to pass one sd,scenario and num_sims since they are all identical
+        if sim_num == 0:
+            rsp.scenario = scenario
+            rsp.num_sims = num_sims
+            rsp.sd = sd
+
+        rsp_list.append(rsp)
+        sim_num += 1
+
+    # ...then do the AssetClasses
+    asset_class_list = AssetClass.query.order_by(AssetClass.title.asc()).all()
+    for j, asset_class in enumerate(asset_class_list):
+        scenario.asset_mix_id = asset_class.id
+        rsp = RunSimParams()
+        rsp.sim_num = sim_num
+        rsp.investments = get_invest_data(scenario.asset_mix_id, assetmix)
+        rsp.type = 'Asset Class'
+        rsp.title = asset_class.title
+
+        rsp_list.append(rsp)
+        sim_num += 1
+
+    job = q.enqueue(_run_all_sim_background, rsp_list, job_timeout=6000)
+
     if sd.debug:
         registry = FailedJobRegistry(queue=q)
 
@@ -79,58 +122,23 @@ def run_all_sim_background(scenario, assetmix):
 
 
 # This runs in an rq worker background process
-def _run_all_sim_background(scenario, assetmix):
-    from Simulation.config import Config
-    from Simulation import create_app
-    from rq import get_current_job
+def _run_all_sim_background(rsp_list):
 
-    flask_app = create_app(Config)
-    flask_app.app_context().push()
-
-    job = get_current_job()
-    sd = SimData.query.first()
-
-    column_names = ['AssetMix Title', 'Type', 'P0', '50th % Final Nestegg']
+    # Initialize a blank dataframe
+    column_names = ['Title', 'Type', 'P0', '50th % Final Nestegg']
     df = pd.DataFrame(columns=column_names)
 
-    # These are used by _run_simulation to determine progress
-    num_sims = len(AssetMix.query.all()) + len(AssetClass.query.all())
-    sim_num = 0
+    for i, rsp in enumerate(rsp_list):
+        rsp.sd = rsp_list[0].sd
+        rsp.scenario = rsp_list[0].scenario
+        rsp.num_sims = rsp_list[0].num_sims
 
-    # First do the AssetMixes
-    asset_mix_list = AssetMix.query.order_by(AssetMix.title.asc()).all()
-    for i, asset_mix in enumerate(asset_mix_list):
-        scenario.asset_mix_id = asset_mix.id
-# Fixme - It should not be possible to SAVE an asset_mix unless it has a title
-        if asset_mix.title == None:
-            pass
-        else:
-            # Get the investment
-            investments = get_invest_data(scenario.asset_mix_id, assetmix)
-
-            p0_output, fd_output = _run_simulation(scenario, investments, sd, num_sims, sim_num, job)
-
-            year = p0_output.shape[0] - 1
-            fd_output[year].sort()
-            df.loc[i] = [asset_mix.title, 'Asset Mix', p0_output[year], fd_output[year][int(sd.num_exp / 2)]]
-
-            sim_num += 1
-
-    # ...then do the AssetClasses
-    asset_class_list = AssetClass.query.order_by(AssetClass.title.asc()).all()
-    for j, asset_class in enumerate(asset_class_list):
-        scenario.asset_mix_id = asset_class.id
-        # Get the investment
-        investments = get_invest_data(scenario.asset_mix_id, assetmix)
-        p0_output, fd_output = _run_simulation(scenario, investments, sd, num_sims, sim_num, job)
-
+        p0_output, fd_output = _run_simulation(rsp)
         year = p0_output.shape[0] - 1
         fd_output[year].sort()
-        df.loc[i + j + 1] = [asset_class.title, 'Asset Class', p0_output[year], fd_output[year][int(sd.num_exp / 2)]]
+        df.loc[i] = [rsp.title, rsp.type, p0_output[year], fd_output[year][int(rsp_list[0].sd.num_exp / 2)]]
 
-        sim_num += 1
-
-    return scenario, df
+    return rsp_list[0].scenario, df
 
 
 # Returns a random sequence of numbers of length num that are randomly drawn from the specified normal distribution
@@ -139,17 +147,19 @@ def sim(mean, stddev, num):
 
 
 def _run_simulation(rsp):
+    from rq import get_current_job
+
+    job = get_current_job()
 
     # Unpack the parameters
     scenario = rsp.scenario
     sd = rsp.sd
     investments = rsp.investments
-    job = rsp.job
     num_sims = rsp.num_sims
     sim_num = rsp.sim_num
 
     # Number of years to simulate
-    if rsp.scenario.has_spouse:
+    if scenario.has_spouse:
         n_yrs = max(scenario.lifespan_age - scenario.current_age, scenario.s_lifespan_age - scenario.s_current_age)
     else:
         n_yrs = scenario.lifespan_age - scenario.current_age
